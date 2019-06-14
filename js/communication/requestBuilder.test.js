ca7621372980133055d447f8ab35c4b40a147ce8
Patrick Desmarais <patrick.desm@gmail.com>
2019-06-14 15:39:23 -0400

    Add two tests for Request.

--- js/communication/requestBuilder.test.js
+++ js/communication/requestBuilder.test.js
@@ -53,70 +53,84 @@
 
         var deleteTest = testSuite.test("DELETE request", function () {
             requestBuilder.delete("/notes/1")
-            .send(function (err, response) {
-                try {
-                    expect(err).toBeNull();
-                    expect(response).toBeObject();
-                    deleteTest.success();
-                } catch (expectError) {
-                    deleteTest.fail(expectError.stack.toString());
-                }
-            });
+                .send(function (err, response) {
+                    try {
+                        expect(err).toBeNull();
+                        expect(response).toBeObject();
+                        deleteTest.success();
+                    } catch (expectError) {
+                        deleteTest.fail(expectError.stack.toString());
+                    }
+                });
         });
         
         var getTest = testSuite.test("GET request", function () {
             requestBuilder.get("/notes/1")
-            .send(function (err, response) {
-                try {
-                    expect(err).toBeNull();
-                    expect(response).toBeObject();
-                    getTest.success();
-                } catch (expectError) {
-                    getTest.fail(expectError.stack.toString());
-                }
-            });
+                .send(function (err, response) {
+                    try {
+                        expect(err).toBeNull();
+                        expect(response).toBeObject();
+                        getTest.success();
+                    } catch (expectError) {
+                        getTest.fail(expectError.stack.toString());
+                    }
+                });
         });
 
         var getWithParamsTest = testSuite.test("GET request (with parameters)", function () {
             requestBuilder.get("/notes")
-            .addQueryParameter("limit", 10)
-            .addQueryParameter("offset", 0)
-            .send(function (err, response) {
-                try {
-                    expect(err).toBeNull();
-                    expect(response).toBeObject();
-                    getWithParamsTest.success();
-                } catch (expectError) {
-                    getWithParamsTest.fail(expectError.stack.toString());
-                }
-            });
+                .addQueryParameter("limit", 10)
+                .addQueryParameter("offset", 0)
+                .send(function (err, response) {
+                    try {
+                        expect(err).toBeNull();
+                        expect(response).toBeObject();
+                        getWithParamsTest.success();
+                    } catch (expectError) {
+                        getWithParamsTest.fail(expectError.stack.toString());
+                    }
+                });
         });
 
         var errorTest = testSuite.test("GET request (with server error)", function () {
             errorRequestBuilder.get("serverReturnsBadRequest")
-            .send(function (err, response) {
-                try {
-                    expect(err).toNotBeNull();
-                    errorTest.success();
-                } catch (expectError) {
-                    errorTest.fail(expectError.stack.toString());
-                }
-            });
+                .send(function (err, response) {
+                    try {
+                        expect(err).toNotBeNull();
+                        errorTest.success();
+                    } catch (expectError) {
+                        errorTest.fail(expectError.stack.toString());
+                    }
+                });
         });
 
-        /*
-        var body = { someKey: "someValue" };
-        var postRequest = Request.post("/notes", body)
-            .send(function (err, response) {
-                // Err is null.
-                // Response is a json.
-            });
-        var putRequest = Request.put("/notes/1", body)
-            .send(function (err, response) {
-                // Err is null.
-                // Response is a json.
-            });
-            */
+        var postBody = JSON.stringify({ someKey: "someValue" });
+        var postTest = testSuite.test("POST request", function () {
+            requestBuilder.post("/notes", postBody)
+                .send(function (err, response) {
+                    try {
+                        expect(err).toBeNull();
+                        expect(response).toBeObject();
+                        postTest.success();
+                    } catch (expectError) {
+                        postTest.fail(expectError.stack.toString());
+                    }
+                });
+        });
+
+        var putBody = JSON.stringify({ id: 1, someKey: "someValue" });
+        var putTest = testSuite.test("PUT request", function () {
+            requestBuilder.post("/notes/1", putBody)
+                .send(function (err, response) {
+                    try {
+                        expect(err).toBeNull();
+                        expect(response).toBeObject();
+                        putTest.success();
+                    } catch (expectError) {
+                        putTest.fail(expectError.stack.toString());
+                    }
+                });
+        });
 
         testSuite.end();
 
