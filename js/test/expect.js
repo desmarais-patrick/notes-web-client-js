2c8072c68ff7279ed888c9fe41d636014b010e1d
Patrick Desmarais <patrick.desm@gmail.com>
2019-07-02 10:29:02 -0400

    Add readability in error messages for expect.

--- js/test/expect.js
+++ js/test/expect.js
@@ -6,37 +6,37 @@
 
         that.toEqual = function (value2) {
             if (value1 !== value2) {
-                throw new Error("'" + value1 + "' does NOT equal '" + value2 + "'");
+                throw new Error("[ASSERTION FAILED] '" + value1 + "' does NOT equal '" + value2 + "'");
             }
         };
 
         that.toBeGreaterThan = function (value2) {
             if (value1 <= value2) {
-                throw new Error("'" + value1 + "' is NOT greater than '" + value2 + "'");
+                throw new Error("[ASSERTION FAILED] '" + value1 + "' is NOT greater than '" + value2 + "'");
             }
         }
 
         that.toBeDefined = function () {
             if (typeof value1 === "undefined") {
-                throw new Error("'" + value1 + "' is NOT defined");
+                throw new Error("[ASSERTION FAILED] '" + value1 + "' is NOT defined");
             }
         }
 
         that.toBeNull = function () {
             if (value1 !== null) {
-                throw new Error("'" + value1 + "' is NOT null");
+                throw new Error("[ASSERTION FAILED] '" + value1 + "' is NOT null");
             }
         }
 
         that.toNotBeNull = function () {
             if (value1 === null) {
-                throw new Error("'" + value1 + "' is null");
+                throw new Error("[ASSERTION FAILED] '" + value1 + "' is null");
             }
         }
 
         that.toBeObject = function () {
             if (typeof value1 !== "object") {
-                throw new Error("'" + value1 + "' is NOT an object");
+                throw new Error("[ASSERTION FAILED] '" + value1 + "' is NOT an object");
             }
         }
 
